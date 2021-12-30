86f10a4205d25f602aeff88aeb2677ee346f00e2
onerandomusername <genericusername414+git@gmail.com>
2021-12-30 12:06:44 -0500

    fix: use standard headers in checkAuth

--- monty-db/src/tokens.ts
+++ monty-db/src/tokens.ts
@@ -1,8 +1,10 @@
+import { HEADERS } from './headers'
+
 export async function checkAuth(request: Request): Promise<Response | boolean> {
   if (!request.headers.has('Authorization')) {
     return new Response(
       JSON.stringify({ error: 'Authorization is required' }),
-      { status: 401 },
+      { status: 401, headers: HEADERS },
     )
   }
   const token = MONTY_DB_TOKEN
@@ -12,7 +14,7 @@ export async function checkAuth(request: Request): Promise<Response | boolean> {
   } else {
     return new Response(
       JSON.stringify({ error: 'You are not permitted to make this action.' }),
-      { status: 403 },
+      { status: 403, headers: HEADERS },
     )
   }
 }
